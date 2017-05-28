6deec2b07fb8d89837a97dfd242d4e5e5b6da056
pishchynski <pavelalexbel@outlook.com>
2017-05-28 12:13:54 +0300

    added experiment_2

--- experiments/experiment_main.py
+++ experiments/experiment_main.py
@@ -4,12 +4,12 @@ from src.experiments import *
 from src.utils import characteristics_loc
 
 
-def run_test(verbose=False):
-    queueing_system = ColdReserveQueueingSystem(name="System_Main_Final", p_num=300)
-    # queueing_system.set_BMAP_queries_stream(matrD_0=np.array([[-86., 0.01], [0.02, -2.76]]) / 57.2,
-    #                                         matrD=np.array([[85., 0.99], [0.2, 2.54]]) / 57.2,
-    #                                         n=3,
-    #                                         q=0.8)
+def run_test_1(verbose=False):
+    queueing_system = ColdReserveQueueingSystem(name="System_Main_Final_1", p_num=300)
+    queueing_system.set_BMAP_queries_stream(matrD_0=np.array([[-86., 0.01], [0.02, -2.76]]) / 57.2,
+                                            matrD=np.array([[85., 0.99], [0.2, 2.54]]) / 57.2,
+                                            n=3,
+                                            q=0.8)
     # queueing_system.set_BMAP_queries_stream(np.array([[-6.3408, 1.87977 * (10 ** (-6))], [1.87977 * (10 ** (-6)), -0.13888]]) / 8.7,
     #                                         np.array([[6.3214, 0.01939], [0.10822, 0.03066]]) / 8.7,
     #                                         q=0.8,
@@ -18,7 +18,7 @@ def run_test(verbose=False):
     #                                         np.array([[5., 0.99], [0.2, 2.54]]) * 5.32,
     #                                         q=0.8,
     #                                         n=3)
-    queueing_system.set_MAP_queries_stream(np.array([[-0.575]]), np.array([[0.575]]))
+    # queueing_system.set_MAP_queries_stream(np.array([[-0.575]]), np.array([[0.575]]))
     queueing_system.set_MAP_break_stream(np.array([[-8.110725, 0.], [0., -0.26325]]),
                                          np.array([[8.0568, 0.053925], [0.146625, 0.116625]]))
     # queueing_system.set_MAP_break_stream(np.array([[-6.3408, 1.87977 / (10**6)], [1.87977 / (10**6), -0.13888]]),
@@ -40,11 +40,44 @@ def run_test(verbose=False):
     return queueing_system
 
 
+def run_test_2(verbose=False):
+    queueing_system = ColdReserveQueueingSystem(name="System_Main_Final_2", p_num=300)
+
+    queueing_system.set_BMAP_queries_stream(np.array([[-8.110725, 0.], [0., -0.26325]]) / 10.5,
+                                            np.array([[8.0568, 0.053925], [0.146625, 0.116625]]) / 10.5,
+                                            q=0.8,
+                                            n=3)
+
+    queueing_system.set_MAP_break_stream(np.array([[-6.3408, 1.87977 / (10**6)], [1.87977 / (10**6), -0.13888]]),
+                                         np.array([[6.3214, 0.01939], [0.10822, 0.03066]]))
+    # queueing_system.set_MAP_break_stream(np.array([[-86., 0.01], [0.02, -2.76]]) * 1.57 / 2.4,
+    #                                      np.array([[85., 0.99], [0.2, 2.54]]) * 1.57 / 2.4)
+    # queueing_system.set_MAP_break_stream(np.array([[-4.5]]), np.array([[4.5]]))
+    queueing_system.set_PH_serv_unit1_stream(np.array([[1., 0.]]), np.array([[-20., 20.], [0., -20.]]))
+    queueing_system.set_PH_serv_unit2_stream(np.array([[1., 0.]]), np.array([[-2., 2.], [0., -2.]]))
+    queueing_system.set_PH_switch1_2_stream(np.array([[0.05, 0.95]]), np.array([[-1.86075, 0.], [0., -146.9994]]))
+    queueing_system.set_PH_switch2_1_stream(np.array([[0.05, 0.95]]), np.array([[-18.6075, 0.], [0., -1469.994]]))
+    queueing_system.set_PH_repair_stream(np.array([[0.98, 0.02]]), np.array([[-100., 0.], [0., -0.002]]))
+    characteristics, vect_p_l = queueing_system.calc_characteristics(verbose=verbose)
+
+    for i, vect in enumerate(vect_p_l):
+        print("P_{} = ".format(str(i)), np.sum(vect))
+
+    for i, charact in enumerate(characteristics):
+        print("{}. ".format(str(i)), characteristics_loc[i], ':', charact)
+
+    return queueing_system
+
+
 if __name__ == '__main__':
-    q_system = run_test(verbose=True)
     read_file = False
+
+    # q_system = run_test(verbose=True)
     # experiment_1(q_system, read_file=read_file)
-    # experiment_2(q_system, read_file=read_file)
+    # experiment_1_1(q_system, read_file=read_file)
+
+    q_system = run_test_2(verbose=True)
+    experiment_2_1(q_system, read_file=read_file)
     # experiment_3(q_system, read_file=read_file)
     # experiment_4(q_system, read_file=read_file)
     # experiment_5(q_system, read_file=read_file)
--- src/experiments.py
+++ src/experiments.py
@@ -153,3 +153,218 @@ def experiment_1(queueing_system: ColdReserveQueueingSystem, read_file=False):
                r'$\bar v$',
                '$c_{cor}$',
                'experiment_1')
+
+
+def experiment_1_1(queueing_system: ColdReserveQueueingSystem, read_file=False):
+    """
+    Зависимость L от \lambda при различных коэффициентах корреляции c_{cor} во входящем потоке
+
+    :param queueing_system: ColdReserveQueueingSystem
+    :param read_file: boolean, if True, read data for plotting from qsr file. False is default
+    :return:
+    """
+
+    linux_check_cpu_temperature()
+
+    print('Experiment 1_1 launched!')
+
+    experiment_1_1_result_list = []
+
+    if not read_file:
+        try:
+            local_queueing_system = copy.deepcopy(queueing_system)
+
+            queries_matrices = copy.deepcopy(local_queueing_system.queries_stream.transition_matrices)
+
+            for cor_coef in range(3):
+                linux_check_cpu_temperature()
+
+                queries_matrices_0 = copy.deepcopy(queries_matrices)
+
+                if cor_coef == 1:
+                    matrD_0_g = np.array([[-6.3408, 1.87977 * (10 ** (-6))], [1.87977 * (10 ** (-6)), -0.13888]]) / 17.4
+                    matrD_g = np.array([[6.3214, 0.01939], [0.10822, 0.03066]]) / 17.4
+
+                    local_queueing_system.set_BMAP_queries_stream(matrD_0_g, matrD_g,
+                                                                  q=local_queueing_system.queries_stream.q,
+                                                                  n=local_queueing_system.n)
+                    queries_matrices_0 = copy.deepcopy(local_queueing_system.queries_stream.transition_matrices)
+                elif cor_coef == 2:
+                    # MAP queries stream
+                    matrD_0_map = np.array([[-0.575]])
+                    matrD_1_map = np.array([[0.575]])
+                    local_queueing_system.set_MAP_queries_stream(matrD_0_map, matrD_1_map)
+                    queries_matrices_0 = copy.deepcopy(local_queueing_system.queries_stream.transition_matrices)
+
+                characteristics, vect_p_l = local_queueing_system.calc_characteristics(verbose=False)
+
+                filename = '../experiment_results/' + 'experiment_1_1_' + queueing_system.name + '.qsc'
+                local_queueing_system.print_characteristics(filename)
+                with open(filename, mode="a") as file:
+                    for i, vect in enumerate(vect_p_l):
+                        print("P_{} = ".format(str(i)), np.sum(vect), file=file)
+
+                    for i, charact in enumerate(characteristics):
+                        print("{}. ".format(str(i)), characteristics_loc[i], ':', charact, file=file)
+                    print("============== END SYSTEM =================\n", file=file)
+
+                experiment_1_1_sublist = [local_queueing_system.queries_stream.c_cor, []]
+
+                output_table = BeautifulTable()
+                output_table.column_headers = ['\\rho', '\\lambda', 'L']
+
+                for queries_coef in tqdm([i / 12 for i in range(1, 25)]):
+                    linux_check_cpu_temperature(notify=False)
+
+                    queries_matrices_1 = [matr * queries_coef for matr in queries_matrices_0]
+
+                    if cor_coef != 2:
+                        local_queueing_system.queries_stream.set_transition_matrices(queries_matrices_1)
+                    else:
+                        local_queueing_system.set_MAP_queries_stream(queries_matrices_1[0], queries_matrices_1[1])
+
+                    characteristics, vect_p_l = local_queueing_system.calc_characteristics(verbose=False)
+
+                    experiment_1_1_sublist[1].append([local_queueing_system.queries_stream.avg_intensity,
+                                                     characteristics[2]])
+                    output_table.append_row([characteristics[0], local_queueing_system.queries_stream.avg_intensity, characteristics[2]])
+
+                with open(filename, mode="a") as file:
+                    print("c_{cor} = ", local_queueing_system.queries_stream.c_cor, file=file)
+                    print("", file=file)
+                    print(output_table, file=file)
+                    print('\n', file=file)
+
+                experiment_1_1_result_list.append(copy.deepcopy(experiment_1_1_sublist))
+
+            file_name = 'experiment_1_1_' + local_queueing_system.name + '.qsr'
+            with open('../experiment_results/' + file_name, mode='w') as res_file:
+                res_file.write(str(experiment_1_1_result_list))
+        except ValueError as e:
+            print(str(e))
+            traceback.print_exc(file=sys.stderr)
+            file_name = 'experiment_1_1_except_' + queueing_system.name + '.qsr'
+            with open('../experiment_results/' + file_name, mode='w') as res_file:
+                res_file.write(str(experiment_1_1_result_list))
+        except Exception:
+            traceback.print_exc(file=sys.stderr)
+            file_name = 'experiment_1_1_except_' + queueing_system.name + '.qsr'
+            with open('../experiment_results/' + file_name, mode='w') as res_file:
+                res_file.write(str(experiment_1_1_result_list))
+    else:
+        file_name = 'experiment_1_1_' + queueing_system.name + '.qsr'
+        with open('../experiment_results/' + file_name, mode='r') as res_file:
+            res_line = res_file.readline()
+        experiment_1_1_result_list = ast.literal_eval(res_line)
+
+    build_plot(experiment_1_1_result_list,
+               r'Зависимость $L$ от $\lambda$ при различных' + '\nкоэффициентах корреляции $c_{cor}$ во входящем потоке',
+               r'$\lambda$',
+               r'$L$',
+               '$c_{cor}$',
+               'experiment_1_1')
+
+
+def experiment_2_1(queueing_system: ColdReserveQueueingSystem, read_file=False):
+    """
+    Зависимость P_1^{+} от h при различных коэффициентах корреляции c_{cor} в потоке поломок
+
+    :param queueing_system: ColdReserveQueueingSystem
+    :param read_file: boolean, if True, read data for plotting from qsr file. False is default
+    :return:
+    """
+
+    linux_check_cpu_temperature()
+
+    print('Experiment 2_1 launched!')
+
+    experiment_2_1_result_list = []
+
+    if not read_file:
+        try:
+            local_queueing_system = copy.deepcopy(queueing_system)
+
+            for cor_coef in range(3):
+                linux_check_cpu_temperature()
+
+                break_matrices_0 = copy.deepcopy(local_queueing_system.break_stream.transition_matrices)
+
+                if cor_coef == 1:
+                    matrH_0 = np.array([[-6.3408, 1.87977 * (10 ** (-6))], [1.87977 * (10 ** (-6)), -0.13888]]) * 1.7 / 2.4
+                    matrH_1 = np.array([[6.3214, 0.01939], [0.10822, 0.03066]]) * 1.7 / 2.4
+
+                    local_queueing_system.set_MAP_break_stream(matrH_0, matrH_1)
+                    break_matrices_0 = copy.deepcopy(local_queueing_system.break_stream.transition_matrices)
+                elif cor_coef == 2:
+                    # MAP queries stream
+                    matrH_0 = np.array([[-4.5]])
+                    matrH_1 = np.array([[4.5]])
+                    local_queueing_system.set_MAP_break_stream(matrH_0, matrH_1)
+                    break_matrices_0 = copy.deepcopy(local_queueing_system.break_stream.transition_matrices)
+
+                characteristics, vect_p_l = local_queueing_system.calc_characteristics(verbose=False)
+
+                filename = '../experiment_results/' + 'experiment_2_1_' + queueing_system.name + '.qsc'
+                local_queueing_system.print_characteristics(filename)
+                with open(filename, mode="a") as file:
+                    for i, vect in enumerate(vect_p_l):
+                        print("P_{} = ".format(str(i)), np.sum(vect), file=file)
+
+                    for i, charact in enumerate(characteristics):
+                        print("{}. ".format(str(i)), characteristics_loc[i], ':', charact, file=file)
+                    print("============== END SYSTEM =================\n", file=file)
+
+                experiment_2_1_sublist = [local_queueing_system.break_stream.c_cor, []]
+
+                output_table = BeautifulTable()
+                output_table.column_headers = ['\\rho', 'h', 'P_1^{+}']
+
+                for queries_coef in tqdm([i / 15 for i in range(1, 37)]):
+                    linux_check_cpu_temperature(notify=False)
+
+                    break_matrices_1 = [matr * queries_coef for matr in break_matrices_0]
+
+                    local_queueing_system.set_MAP_break_stream(break_matrices_1[0], break_matrices_1[1])
+
+                    characteristics, vect_p_l = local_queueing_system.calc_characteristics(verbose=False)
+
+                    experiment_2_1_sublist[1].append([local_queueing_system.break_stream.avg_intensity,
+                                                     characteristics[4]])
+                    output_table.append_row([characteristics[0],
+                                             local_queueing_system.break_stream.avg_intensity,
+                                             characteristics[4]])
+
+                with open(filename, mode="a") as file:
+                    print("c_{cor} = ", local_queueing_system.break_stream.c_cor, file=file)
+                    print("", file=file)
+                    print(output_table, file=file)
+                    print('\n', file=file)
+
+                experiment_2_1_result_list.append(copy.deepcopy(experiment_2_1_sublist))
+
+            file_name = 'experiment_2_1_' + local_queueing_system.name + '.qsr'
+            with open('../experiment_results/' + file_name, mode='w') as res_file:
+                res_file.write(str(experiment_2_1_result_list))
+        except ValueError as e:
+            print(str(e))
+            traceback.print_exc(file=sys.stderr)
+            file_name = 'experiment_2_1_except_' + queueing_system.name + '.qsr'
+            with open('../experiment_results/' + file_name, mode='w') as res_file:
+                res_file.write(str(experiment_2_1_result_list))
+        except Exception:
+            traceback.print_exc(file=sys.stderr)
+            file_name = 'experiment_2_1_except_' + queueing_system.name + '.qsr'
+            with open('../experiment_results/' + file_name, mode='w') as res_file:
+                res_file.write(str(experiment_2_1_result_list))
+    else:
+        file_name = 'experiment_2_1_' + queueing_system.name + '.qsr'
+        with open('../experiment_results/' + file_name, mode='r') as res_file:
+            res_line = res_file.readline()
+        experiment_2_1_result_list = ast.literal_eval(res_line)
+
+    build_plot(experiment_2_1_result_list,
+               r'Зависимость $P_1^{+}$ от $h$ при различных' + '\nкоэффициентах корреляции $c_{cor}$ в потоке поломок',
+               r'h',
+               r'$P_1^{+}$',
+               '$c_{cor}$',
+               'experiment_2_1')
